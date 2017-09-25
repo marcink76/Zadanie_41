6bd4db1b54a55cb3548ba7f28f1b9716b051324c
Marcin Konieczny <marcin_k@coni.pl>
2017-09-25 11:40:32 +0200

    Version 2.0

--- src/InputData.java
+++ src/InputData.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 public class InputData {
 
-    public static void inputData(Triangle triangle) {
+    public static Triangle inputData() {
+
+        Triangle triangle = new Triangle();
+
         Scanner sideRead = new Scanner(System.in);
         System.out.println("Podaj pierwszy bok trójkąta:");
         triangle.setSideA(sideRead.nextDouble());
@@ -10,5 +13,7 @@ public class InputData {
         triangle.setSideB(sideRead.nextDouble());
         System.out.println("Podaj trzeci bok trójkąta:");
         triangle.setSideC(sideRead.nextDouble());
+
+        return triangle;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,11 @@ import java.util.Locale;
 
 public class Main {
     public static void main (String[] args){
-        Triangle triangle = new Triangle();
-        InputData.inputData(triangle);
-        if (CheckTriangle.isTriangleRight(triangle)){
-            System.out.printf(Locale.ROOT,"Trójkąt o bokach:\n A: %.2f\n B: %.2f\n C: %.2f\nJest prostokątny!",
-                    triangle
-                    .getSideA(), triangle.getSideB(), triangle.getSideC());
+
+        if (CheckTriangle.isTriangleRight(InputData.inputData())){
+            System.out.println("Jest prostokątny!");
         }else {
-            System.out.printf(Locale.ROOT,"Trójkąt o bokach:\n A: %.2f\n B: %.2f\n C: %.2f\nNie jest prostokątny!",
-                    triangle
-                    .getSideA(), triangle.getSideB(), triangle.getSideC());
+            System.out.println("Nie jest prostokątny");
         }
 
     }
